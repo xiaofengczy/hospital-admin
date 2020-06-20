e41e63491ba09f1e483c07cf5bc54b415e4ffd39
caozhongyu <caozhongyu@yunsom.com>
2020-06-20 09:28:30 +0800

    完成注册登陆功能

--- app.js
+++ app.js
@@ -1,48 +1,63 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
 
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var adminRouter = require('./routes/admin/adminRouter')
-var authRouter = require('./routes/auth/authRouter.js')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var adminRouter = require("./routes/admin/adminRouter");
+var authRouter = require("./routes/auth/authRouter.js");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(
+  session({
+    // 可以随便写。 一个 String 类型的字符串，作为服务器端生成 session 的签名
+    secret: "this is hospital key",
+    //强制保存 session 即使它并没有变化,。默认为 true。建议设置成 false
+    resave: false,
+    //强制将未初始化的 session 存储。  默认值是true  建议设置成true
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 5000 /*过期时间*/,
+    },
+    //设置过期时间比如是30分钟，只要游览页面，30分钟没有操作的话在过期
+    rolling: true, //在每次请求时强行设置 cookie，这将重置 cookie 过期时间（默认：false）
+  })
+);
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 //后台管理
-app.use('/admin',adminRouter);
+app.use("/admin", adminRouter);
 //登陆注册
-app.use('/auth',authRouter)
+app.use("/auth", authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/auth/authRouter.js
+++ routes/auth/authRouter.js
@@ -1,52 +1,91 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var sqlQuery = require('../../utils/mysqlUtil');
+var sqlQuery = require("../../utils/mysqlUtil");
+var crypto = require("crypto");
 
+function encryption(str, salt) {
+  let obj = crypto.createHash("md5");
+  str = salt + str;
+  obj.update(str);
+  return obj.digest("hex");
+}
+
+function getRandomSalt() {
+  return Math.random().toString(16).slice(2, 5);
+}
 
 /* 登陆页面 */
-router.get('/login', function(req, res, next) {
-  res.render('auth/login.ejs');
+router.get("/login", function (req, res, next) {
+  res.render("auth/login.ejs");
 });
-
 
 /* 注册 */
-router.get('/register', function(req, res, next) {
-  res.render('auth/register.ejs');
+router.get("/register", function (req, res, next) {
+  res.render("auth/register.ejs");
 });
-
 
 /* 登陆逻辑 */
-router.post('/login', function(req, res, next) {
+router.post("/login", async function (req, res, next) {
   let username = req.body.username;
   let password = req.body.password;
-  let sql = 'select * from user where username=? and password=?';
-});
-
-/* 注册逻辑 */
-router.post('/register',async function(req, res, next) {
-  let username = req.body.username;
-  let password = req.body.password;
-  let sql = 'select * from t_user where username = ?';
-  let result = await sqlQuery(sql,[username]);
-  if(result.length!==0){
-    //已经注册
-    res.render('info/error.ejs',{
-      title:'注册失败',
-      content:'用户已存在',
-      hrefText:'注册页',
-      href:'/auth/register'
-    })
-  }else{
-    sql = 'insert into t_user (username,password) values (?,?)';
-    result = await sqlQuery(sql,[username,password]);
-    res.render('info/error.ejs',{
-      title:'注册成功',
-      content:'恭喜您,注册成功',
-      hrefText:'登陆页',
-      href:'/auth/login'
-    })
+  //根据账号查询用户
+  let sql = "select * from t_user where username = ?";
+  let result = await sqlQuery(sql, [username]);
+  if (result.length !== 0) {  
+    if (result[0].password === encryption(password, result[0].salt)) {
+      req.session.username = username;
+      //登陆成功
+      res.render("info/error.ejs", {
+        title: "登陆成功",
+        content: "恭喜您，登陆成功",
+        hrefText: "主页",
+        href: "/admin",
+      });
+    } else {
+      //登陆失败
+      res.render("info/error.ejs", {
+        title: "登陆失败",
+        content: "账号或密码错误",
+        hrefText: "登陆页",
+        href: "/auth/login",
+      });
+    }
+  } else {
+    //登陆失败
+    res.render("info/error.ejs", {
+      title: "登陆失败",
+      content: "账号或密码错误",
+      hrefText: "登陆页",
+      href: "/auth/login",
+    });
   }
 });
 
+/* 注册逻辑 */
+router.post("/register", async function (req, res, next) {
+  let username = req.body.username;
+  let password = req.body.password;
+  let sql = "select * from t_user where username = ?";
+  let result = await sqlQuery(sql, [username]);
+  if (result.length !== 0) {
+    //已经注册
+    res.render("info/error.ejs", {
+      title: "注册失败",
+      content: "用户已存在",
+      hrefText: "注册页",
+      href: "/auth/register",
+    });
+  } else {
+    sql = "insert into t_user (username,password,salt) values (?,?,?)";
+    let salt = getRandomSalt();
+    result = await sqlQuery(sql, [username, encryption(password, salt), salt]);
+    res.render("info/error.ejs", {
+      title: "注册成功",
+      content: "恭喜您,注册成功",
+      hrefText: "登陆页",
+      href: "/auth/login",
+    });
+  }
+});
 
 module.exports = router;
